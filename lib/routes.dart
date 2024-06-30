07ac2d5f63baac83e8e5429b673bbea3ad4624ca
RJ2607 <rishijain2607@gmail.com>
2024-06-30 21:57:11 +0530

    refactor: Update routes.dart imports and add new routes
    This commit updates the imports in the `routes.dart` file to match the new file structure. It also adds new routes for the `NavBarMenu`, `MyTrip`, `Maps`, and `Profile` screens. These changes improve the organization of the codebase and add new functionality to the app.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:tripmate/auth/loginPage.dart';
-import 'package:tripmate/views/homeScreen.dart';
+import 'package:tripmate/views/Auth/loginPage.dart';
+import 'package:tripmate/views/Home/homeScreen.dart';
+import 'package:tripmate/views/Maps/mapPage.dart';
+import 'package:tripmate/views/My%20Trip/myTripPage.dart';
+import 'package:tripmate/views/Profile/profilePage.dart';
+
+import 'views/navBarMenu.dart';
+import 'views/onboardingScreen.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => HomeScreen());
+        return MaterialPageRoute(builder: (_) => OnboardingScreen());
       case '/login':
         return MaterialPageRoute(builder: (_) => LoginPage());
       case '/home':
         return MaterialPageRoute(builder: (_) => HomeScreen());
-
+      case '/navigation':
+        return MaterialPageRoute(builder: (_) => NavBarMenu());
+      case '/mytrip':
+        return MaterialPageRoute(builder: (_) => MyTrip());
+      case '/maps':
+        return MaterialPageRoute(builder: (_) => Maps());
+      case '/profile':
+        return MaterialPageRoute(builder: (_) => Profile());
       default:
         return _errorRoute();
     }
