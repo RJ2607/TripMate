0379148e8d26a1636eca6b8812d484c7dafb7ece
RJ2607 <rishijain2607@gmail.com>
2024-08-05 22:52:13 +0530

    feat: Add ActivityModel class for managing activity data
    This commit adds the ActivityModel class to the app. The ActivityModel class represents an activity and its associated data, such as activity name, location, transport, description, and total time. It includes methods for converting the activity data to JSON and creating an instance from JSON data. This addition enhances the app's functionality by providing a dedicated model for managing activity data.

---
+++ lib/models/activityModel.dart
@@ -0,0 +1,51 @@
+class ActivityModel {
+  String activityName;
+  String location;
+  String transport;
+  String description;
+  DateTime totalTime;
+
+  ActivityModel({
+    required this.activityName,
+    required this.location,
+    required this.transport,
+    required this.description,
+    required this.totalTime,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'activityName': activityName,
+      'location': location,
+      'transport': transport,
+      'description': description,
+      'totalTime': totalTime,
+    };
+  }
+
+  factory ActivityModel.fromJson(Map<String, dynamic> data) {
+    return ActivityModel(
+      activityName: data['activityName'],
+      location: data['location'],
+      transport: data['transport'],
+      description: data['description'],
+      totalTime: data['totalTime'].toDate(),
+    );
+  }
+
+  ActivityModel copyWith({
+    String? activityName,
+    String? location,
+    String? transport,
+    String? description,
+    DateTime? totalTime,
+  }) {
+    return ActivityModel(
+      activityName: activityName ?? this.activityName,
+      location: location ?? this.location,
+      transport: transport ?? this.transport,
+      description: description ?? this.description,
+      totalTime: totalTime ?? this.totalTime,
+    );
+  }
+}
