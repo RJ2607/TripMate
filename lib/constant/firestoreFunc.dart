5b07f18b8e2dc37529ec6510962c8b1a7f9efd6b
RJ2607 <rishijain2607@gmail.com>
2024-10-09 00:39:57 +0530

    feat: Refactor Firestore operations to use GetX for state management and enhance functionality

--- lib/constant/firestoreFunc.dart
+++ lib/constant/firestoreFunc.dart
@@ -1,33 +1,147 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:developer';
 
-class FirestoreFunc {
-  static String users = 'users';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+import '../models/userModel.dart';
+
+class FirestoreFunc extends GetxController {
+  User? user = FirebaseAuth.instance.currentUser;
+  String users = 'users';
   static FirebaseFirestore db = FirebaseFirestore.instance;
 
-  static CollectionReference<Map<String, dynamic>> groupTrip =
+  CollectionReference<Map<String, dynamic>> groupTrip =
       db.collection('groupTrip');
-  static CollectionReference<Map<String, dynamic>> get userCollection =>
+  CollectionReference<Map<String, dynamic>> get userCollection =>
       db.collection(users);
-  static CollectionReference<Map<String, dynamic>> trips(String uid) =>
+  CollectionReference<Map<String, dynamic>> trips(String uid) =>
       userCollection.doc(uid).collection('trips');
 
-  static Future<Map<String, dynamic>?> getUserByEmail(String email) async {
+  CollectionReference<Map<String, dynamic>> groupDayActivity(String groupId) =>
+      groupTrip.doc(groupId).collection('dayActivity');
+  CollectionReference<Map<String, dynamic>> individualDayActivity(
+          String tripId) =>
+      trips(user!.uid).doc(tripId).collection('dayActivity');
+
+  Future<Map<String, dynamic>?> getUserByEmail(String email) async {
     return await userCollection
         .where('email', isEqualTo: email)
         .get()
         .then((value) => value.docs.first.data());
   }
 
-  static addIndividualTrip(String uid, Map<String, dynamic> data) {
+  Future getuserByUid(String uid) async {
+    // await Future.delayed(const Duration(seconds: 2));
+    return await userCollection.doc(uid).get().then((value) => value.data());
+  }
+
+  uploadUser(User user) {
     try {
-      trips(uid).add(data);
+      UserModel userModel = UserModel(
+        uid: user.uid,
+        name: user.displayName!,
+        email: user.email!,
+        // phone: user.phoneNumber!,
+        profile: user.photoURL!,
+      );
+      userCollection.doc(user.uid).get().then((doc) {
+        if (!doc.exists) {
+          userCollection.doc(user.uid).set(userModel.toJson());
+        }
+      });
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  updateTrip(String uid, Map<String, dynamic> data) {
+    try {
+      trips(uid).doc(data['tripId']).update(data);
     } catch (e) {
       print(e);
     }
   }
 
-  static Future getuserByUid(String uid) async {
-    // await Future.delayed(const Duration(seconds: 2));
-    return await userCollection.doc(uid).get().then((value) => value.data());
+  updateGroupTrip(String uid, Map<String, dynamic> data) {
+    try {
+      groupTrip.doc(data['groupId']).update(data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  addIndividualTrip(String uid, Map<String, dynamic> data) {
+    try {
+      trips(uid).add(data).then((value) {
+        Get.back();
+        Get.snackbar('Success', 'Trip added successfully');
+
+        return value.id;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  addGroupTrip(Map<String, dynamic> data, String uid) {
+    try {
+      groupTrip.add(data).then((value) {
+        Get.back();
+        addIndividualTrip(
+          uid,
+          {
+            'groupId': value.id,
+            'isGroupTrip': true,
+          },
+        );
+        Get.snackbar('Success', 'Trip added successfully');
+        return value.id;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  getTripsStream() {
+    return trips(FirebaseAuth.instance.currentUser!.uid).snapshots();
+  }
+
+  Future getGroupTripsById(String uid) async {
+    return await groupTrip.doc(uid).get().then((value) => value.data());
+  }
+
+  Future getIndividualTripById(String tripId) async {
+    return await trips(user!.uid)
+        .doc(tripId)
+        .get()
+        .then((value) => value.data());
+  }
+
+  addDayActivity(String tripId, String dayId, String category, bool isGroupTrip,
+      Map<String, dynamic> data) {
+    try {
+      if (isGroupTrip) {
+        groupDayActivity(tripId)
+            .doc(dayId)
+            .collection(category)
+            .add(data)
+            .then((value) {
+          Get.back();
+          Get.snackbar('Success', 'Activity added successfully');
+        });
+      } else {
+        individualDayActivity(tripId)
+            .doc(dayId)
+            .collection(category)
+            .add(data)
+            .then((value) {
+          Get.back();
+          Get.snackbar('Success', 'Activity added successfully');
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
