81b05f4290da352c9bc5d1599031a81bc69d2d93
RJ2607 <rishijain2607@gmail.com>
2024-10-09 00:41:24 +0530

    feat: Implement tabbed navigation in MyTrip page with Individual and Group Trip views

--- lib/Views/My Trip/myTripPage.dart
+++ lib/Views/My Trip/myTripPage.dart
@@ -1,17 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:tripmate/views/My%20Trip/trips%20view/individualTrip.dart';
+
+import 'trips view/groupTrip.dart';
+
+class MyTab extends GetxController {
+  late TabController controller;
+
+  final List<Tab> tabs = [
+    Tab(text: 'Individual Trip'),
+    Tab(text: 'Group Trip'),
+  ];
+
+  void initController(TickerProvider vsync) {
+    controller = TabController(length: tabs.length, vsync: vsync);
+  }
+
+  @override
+  void onClose() {
+    controller.dispose();
+    super.onClose();
+  }
+}
 
 class MyTrip extends StatefulWidget {
-  const MyTrip({super.key});
+  MyTrip({super.key});
 
   @override
   State<MyTrip> createState() => _MyTripState();
 }
 
-class _MyTripState extends State<MyTrip> {
+class _MyTripState extends State<MyTrip> with SingleTickerProviderStateMixin {
+  MyTab myTab = Get.put(MyTab());
+
+  @override
+  void initState() {
+    super.initState();
+    myTab = Get.put(MyTab());
+    myTab.initController(this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        bottom: TabBar(
+          labelColor: Theme.of(context).colorScheme.primaryContainer,
+          unselectedLabelColor: Theme.of(context).colorScheme.onBackground,
+          indicatorColor: Theme.of(context).colorScheme.primaryContainer,
+          controller: myTab.controller,
+          tabs: myTab.tabs,
+        ),
+        title: Text(
+          'My Trips',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor:
             Theme.of(context).floatingActionButtonTheme.backgroundColor,
@@ -26,6 +75,16 @@ class _MyTripState extends State<MyTrip> {
         },
         icon: const Icon(Bootstrap.plus),
       ),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: TabBarView(
+            physics: const ScrollPhysics(),
+            controller: myTab.controller,
+            children: [
+              IndividualTrip(),
+              GroupTrip(),
+            ],
+          )),
     );
   }
 }
