f668d2c5841a7252ae973dae2935fbb8d2aa5042
RJ2607 <rishijain2607@gmail.com>
2024-10-09 00:40:41 +0530

    refactor: Remove unused fields and activity list from TripModel for code simplification

--- lib/models/tripModel.dart
+++ lib/models/tripModel.dart
@@ -1,43 +1,38 @@
-import 'package:tripmate/models/activityModel.dart';
-
 class TripModel {
-  String id;
-  String travelMode;
+  // String id;
+  // String travelMode;
   String destination;
   DateTime startDate;
   DateTime endDate;
   bool isGroup;
   List<String>? invitees;
-  List<ActivityModel>? activity;
 
   TripModel({
-    required this.id,
-    required this.travelMode,
+    // required this.id,
+    // required this.travelMode,
     required this.destination,
     required this.startDate,
     required this.endDate,
     required this.isGroup,
     this.invitees,
-    this.activity,
   });
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'travelMode': travelMode,
+      // 'id': id,
+      // 'travelMode': travelMode,
       'destination': destination,
       'startDate': startDate,
       'endDate': endDate,
       'isGroup': isGroup,
       'invitees': invitees,
-      'activity': activity,
     };
   }
 
   factory TripModel.fromJson(Map<String, dynamic> data) {
     return TripModel(
-      id: data['id'],
-      travelMode: data['travelMode'],
+      // id: data['id'],
+      // travelMode: data['travelMode'],
       destination: data['destination'],
       startDate: data['startDate'].toDate(),
       endDate: data['endDate'].toDate(),
@@ -45,31 +40,26 @@ class TripModel {
       invitees: data['invitees'] != null
           ? List<String>.from(data['invitees'].map((x) => x))
           : null,
-      activity: data['activity'] != null
-          ? List<ActivityModel>.from(data['activity'].map((x) => x))
-          : null,
     );
   }
 
   TripModel copyWith({
-    String? id,
-    String? travelMode,
+    // String? id,
+    // String? travelMode,
     String? destination,
     DateTime? startDate,
     DateTime? endDate,
     bool? isGroup,
     List<String>? invitees,
-    List<ActivityModel>? activity,
   }) {
     return TripModel(
-      id: id ?? this.id,
-      travelMode: travelMode ?? this.travelMode,
+      // id: id ?? this.id,
+      // travelMode: travelMode ?? this.travelMode,
       destination: destination ?? this.destination,
       startDate: startDate ?? this.startDate,
       endDate: endDate ?? this.endDate,
       isGroup: isGroup ?? this.isGroup,
       invitees: invitees ?? this.invitees,
-      activity: activity ?? this.activity,
     );
   }
 }
