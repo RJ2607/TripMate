195a81e6f9b74cc022e5a6ba74cbac0d230a8f05
RJ2607 <rishijain2607@gmail.com>
2024-10-09 00:41:48 +0530

    feat: Refactor AddActivity to improve category selection and integrate TransportFields; update UI components for better usability

--- lib/Views/My Trip/add trip/addActivity.dart
+++ lib/Views/My Trip/add trip/addActivity.dart
@@ -1,38 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tripmate/constant/widgets/timeRangeField.dart';
-import 'package:tripmate/views/My%20Trip/add%20trip/category%20fields/LodgingFields.dart';
-import 'package:tripmate/views/My%20Trip/add%20trip/category%20fields/restaurantFields.dart';
 import 'package:tripmate/views/My%20Trip/add%20trip/category%20fields/sightseeingFields.dart';
-import 'package:tripmate/views/My%20Trip/add%20trip/category%20fields/travelFields.dart';
 
 import '../../../controller/dateRangeController.dart';
+import 'category fields/lodgingFields.dart';
+import 'category fields/restaurantFields.dart';
+import 'category fields/transportField.dart';
 
 class AddActivityController extends GetxController {
   Rx<String> selectedCategory = ''.obs;
   Rx<TextEditingController> activityNameController =
       TextEditingController().obs;
   Rx<TextEditingController> descriptionController = TextEditingController().obs;
+  Rx<TextEditingController> categoryController = TextEditingController().obs;
 }
 
 class AddActivity extends StatelessWidget {
-  final AddActivityController addActivityController =
-      Get.put(AddActivityController());
-  final DateRangePickerController dateRangeController =
-      Get.put(DateRangePickerController());
-
   AddActivity({super.key});
-
+  AddActivityController addActivityController =
+      Get.put(AddActivityController());
+  DateRangePickerController dateRangeController =
+      Get.put(DateRangePickerController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        label: const Text('Add Activity'),
-        onPressed: () {
-          Get.back();
-        },
-      ),
       body: Padding(
         padding: EdgeInsets.only(
           left: MediaQuery.of(context).size.width * 0.05,
@@ -50,7 +41,8 @@ class AddActivity extends StatelessWidget {
                   GestureDetector(
                     child: const Icon(Icons.arrow_back),
                     onTap: () {
-                      dateRangeController.selectedTimeRange.value = null;
+                      dateRangeController.selectedStartTimeRange.value = null;
+                      dateRangeController.selectedEndTimeRange.value = null;
                       Navigator.pop(context);
                     },
                   ),
@@ -69,87 +61,98 @@ class AddActivity extends StatelessWidget {
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.03,
               ),
-              TextField(
-                controller: addActivityController.activityNameController.value,
-                decoration: const InputDecoration(
+              const TextField(
+                decoration: InputDecoration(
                   labelText: 'Activity Name',
                 ),
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.03,
               ),
-              TextField(
-                controller: addActivityController.descriptionController.value,
-                decoration: const InputDecoration(
-                  labelText: 'Description',
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Notes',
                 ),
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.03,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TimeRangePickerWidget(),
-                  Obx(() => DropdownMenu<String>(
-                        initialSelection:
-                            addActivityController.selectedCategory.value,
-                        onSelected: (String? value) {
-                          if (value != null) {
-                            addActivityController.selectedCategory.value =
-                                value;
-                          }
-                        },
-                        enableFilter: true,
-                        label: const Text(
-                          'Category',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        width: MediaQuery.of(context).size.width * 0.55,
-                        inputDecorationTheme: InputDecorationTheme(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        dropdownMenuEntries: const <DropdownMenuEntry<String>>[
-                          DropdownMenuEntry(
-                            value: 'Travel',
-                            label: 'Travel',
-                          ),
-                          DropdownMenuEntry(
-                            value: 'Restaurant',
-                            label: 'Restaurant',
-                          ),
-                          DropdownMenuEntry(
-                            value: 'Sightseeing',
-                            label: 'Sightseeing',
-                          ),
-                          DropdownMenuEntry(
-                            value: 'Lodging',
-                            label: 'Lodging',
-                          ),
-                        ],
-                      )),
-                ],
+              Obx(
+                () => DropdownMenu(
+                    expandedInsets: EdgeInsets.symmetric(
+                      horizontal: 0,
+                    ),
+                    controller: addActivityController.categoryController.value,
+                    enableFilter: true,
+                    onSelected: (value) {
+                      dateRangeController.selectedStartTimeRange.value = null;
+                      dateRangeController.selectedEndTimeRange.value = null;
+                      addActivityController.selectedCategory.value = value!;
+                    },
+                    label: const Text('Category',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        )),
+                    width: MediaQuery.of(context).size.width * 0.55,
+                    inputDecorationTheme: InputDecorationTheme(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    dropdownMenuEntries: const <DropdownMenuEntry<String>>[
+                      DropdownMenuEntry(
+                        value: 'Transport',
+                        label: 'Transport',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'Restaurant',
+                        label: 'Restaurant',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'Sightseeing',
+                        label: 'Sightseeing',
+                      ),
+                      DropdownMenuEntry(
+                        value: 'Lodging',
+                        label: 'Lodging',
+                      ),
+                    ]),
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.03,
               ),
-              Obx(
-                () {
-                  switch (addActivityController.selectedCategory.value) {
-                    case 'Travel':
-                      return TravelFields();
-                    case 'Restaurant':
-                      return RestaurantFields();
-                    case 'Sightseeing':
-                      return SightseeingFields();
-                    case 'Lodging':
-                      return LodgingFields();
-                    default:
-                      return Container();
-                  }
-                },
+              Obx(() {
+                if (addActivityController.selectedCategory.value ==
+                    'Transport') {
+                  return TransportFields();
+                } else if (addActivityController.selectedCategory.value ==
+                    'Sightseeing') {
+                  return const SightseeingFields();
+                } else if (addActivityController.selectedCategory.value ==
+                    'Restaurant') {
+                  return RestaurantFields();
+                } else if (addActivityController.selectedCategory.value ==
+                    'Lodging') {
+                  return LodgingFields();
+                } else {
+                  return const SizedBox();
+                }
+              }),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 15,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: const Text("Add Activity"),
+                ),
               ),
             ],
           ),
